5aefe69199993db0fe51f4c87e9bfd9dcf1b4807
shannon <shannonchimutengo@gmail.com>
2022-06-24 16:01:14 +0200

    added student optionals

--- src/main/java/com/example/demo2/Student/StudentService.java
+++ src/main/java/com/example/demo2/Student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo2.Student;
 
+import com.fasterxml.jackson.annotation.OptBoolean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,6 +22,12 @@ public final StudentRepository studentRepository;
 
 
     public void addNewStudent(Student student) {
-        System.out.println(student);
+        Optional<Student>studentOptional = studentRepository
+                .findStudentsByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()){
+            throw new IllegalMonitorStateException("email is taken");
+        }
+
     }
 }
